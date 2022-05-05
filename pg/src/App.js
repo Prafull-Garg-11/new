064ded17d72a9314e448af43ce122516608cd155
Prafull-Garg-11 <prafullgarg.it19@aryacollege.in>
2022-05-05 21:21:15 +0530

    adding react app

---
+++ pg/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Demo from "./components/demo.jsx"
+function App() {
+  return (
+      <>
+      <h1>Building React Application</h1>
+      <Demo></Demo>
+      </>
+  );
+}
+
+export default App;
